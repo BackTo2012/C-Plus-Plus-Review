01366e202df512f10f29a73896fc30c33b233751
BackTo2012 <1742331636@qq.com>
2021-06-24 23:59:49 +0800

    C++

---
+++ 引用作为函数参数（黄云飞出题，大家可以做一做）.cpp
@@ -0,0 +1,19 @@
+//StudybarCommentBegin
+#include <iostream>
+using namespace std;
+void swap(int &, int &);
+int main( )
+{
+	int i, j;
+	cin >> i >> j ;
+	swap(i, j);
+	cout << i << " " << j << endl;
+	return 0;
+}
+//StudybarCommentEnd
+
+void swap(int &a, int &b) {
+	int t = a;
+	a = b;
+	b = t;
+}
